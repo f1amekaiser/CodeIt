a0836023120cbb71b29f888511966bd9838ef48d
ADITYA <aditya180606s@gmail.com>
2025-07-12 22:25:43 +0530

    Update server.js

--- React/server/server.js
+++ React/server/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 const filepath = path.join(__dirname, 'code.py');
 
